56034f53ecfd53c8fec6f4f2e5ec40e50ea5b8b9
Marceloski1 <140574281+Marceloski1@users.noreply.github.com>
2025-08-10 11:10:11 -0400

    commit 3

--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,20 @@ export default function Home() {
       tipo_sangre: 'O+'
     }
 
+    const p2 = {
+      edad:32 , 
+      nombre:'pedrito' , 
+      tipo_sangre: 'O-'
+    }
+
+
    return (
     <div className="font-sans items-center justify-items-center sm:p-20 ">
      <main className="grid grid-cols-2">
           <div className="m-2 p-2 text-red-400 bg-blue-100 rounded-xl">
             {p.edad}
           </div>
-   
-
-
-
+   </main>
      <footer className="">
       <div>
         <span>Hola como estas </span>
